8cb24f47f0d8bc5ce2593918fafb8d7f86a091b4
Emanuel M <emanuel.munera1@gmail.com>
2025-06-12 12:52:44 -0500

    simple commit to throw another pipeline

--- src/main/java/com/udea/petstore/DataLoader.java
+++ src/main/java/com/udea/petstore/DataLoader.java
@@ -21,7 +21,7 @@ public class DataLoader implements CommandLineRunner {
     /**
     @Override
     public void run(String... args) throws Exception {
-        //throw new UnsupportedOperationException("Esta función no se ha implementado");
+        throw new UnsupportedOperationException("Esta función no se ha implementado");
     }
     */
 
